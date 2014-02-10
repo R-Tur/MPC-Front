5d928944d8b03bca6bde48e05eaf033cf2a22f2e
R-Tur <art.yunusov@gmail.com>
2014-02-10 21:32:30 +0000

    dev

--- auth.php
+++ auth.php
@@ -1,8 +1,7 @@
 <?
-if (!($_SERVER['PHP_AUTH_USER'] == $CONFIG["user"] && $_SERVER['PHP_AUTH_PW'] == $CONFIG["pass"])) {
+if (!(strcmp($_SERVER['PHP_AUTH_USER'],$CONFIG["user"]) == 0 && strcmp($_SERVER['PHP_AUTH_PW'],$CONFIG["pass"]) == 0)) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
-    echo 'Authorization is required!';
     exit;
 } 
 ?>
