4110ab79ac420bc88e66e9929d709556eb3513a5
Artur Iunusov <iunusov.artur@gmail.com>
2015-02-23 00:49:16 +0300

    auth

--- auth.php
+++ auth.php
@@ -1,4 +1,5 @@
 <?
+if($CONFIG["pass"]!="" || $CONFIG["user"]!="")
 if (!(strcmp($_SERVER['PHP_AUTH_USER'],$CONFIG["user"]) == 0 && strcmp($_SERVER['PHP_AUTH_PW'],$CONFIG["pass"]) == 0)) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
